8437621d6e56c6467d0f64279d725776c84283f7
nicholastenbrink <nicholas.tenbrink@gmail.com>
2017-03-13 18:13:46 -0500

    update to add Prowl functionality
    added prowl to allow iOS Push notification

--- sailclockReturn.py
+++ sailclockReturn.py
@@ -7,8 +7,8 @@ import time
 
 
 open_time = 1489579200
-alert_time = 300  # 5min warning
-#alert_time = 220000
+#alert_time = 300  # 5min warning
+alert_time = 132600
 timeLeft = 0
 timer = 0 
 
@@ -25,28 +25,47 @@ headers = {'User-Agent': user_agent}
 data = urllib.urlencode(values)
 
 
-while True:	
-	#this is where the request is
-	req = urllib2.Request(url_source, data, headers)
-	response = urllib2.urlopen(req)
-	
-	#this is the return from timenow
-	timer = int(response.read())
+def main():
+	while True:
+		
+		#this is where the request is
+		req = urllib2.Request(url_source, data, headers)
+		response = urllib2.urlopen(req)
+		
+		#this is the return from timenow
+		timer = int(response.read())
 
-	#this checks how much time left
-	timeLeft = open_time - timer
+		#this checks how much time left
+		timeLeft = open_time - timer
 
-	if (timeLeft < alert_time):
-		print "its time"
-		print timeLeft
-		print timer
+		if (timeLeft < alert_time):
+			print "its time"
+			print timeLeft
+			print timer
+			send_prowl_alert()
 
 
-	else:
-		print "Not there yet, it's still " 
-		print timeLeft
-		print timer
+		else:
+			print "Not there yet, it's still " 
+			print timeLeft
+			print timer
 
-	#sleeps the script for min
-	time.sleep(600)	
+		#sleeps the script for min
+		time.sleep(600)	
 
+def send_prowl_alert():
+    prowl_url = 'https://prowlapp.com/publicapi/add'
+    prowl_api_keys = [
+        'xxxxxxx'  # nick
+        ]
+    sailsignup_url = 'https://tcsailing.org/harbor/application/'
+    for prowl_user_key in prowl_api_keys:
+        payload = {
+                    'apikey': prowl_user_key,
+                    'application': 'Sailing Signup',
+                    'description': 'Go sign up!\n{}'.format(sailsignup_url),
+                    'priority': 2,
+                   }
+        requests.get(prowl_url, params=payload)
+
+main()
